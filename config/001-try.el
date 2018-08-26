cf2a2b67b213cda07782024a39a607935eae2c01
Alen Alex Ninan <alenalexninan@gmail.com>
2018-08-26 22:36:36 +0530

    Initial commit to add all the current files

---
+++ config/001-try.el
@@ -0,0 +1,3 @@
+;; Install try
+(use-package try
+	:ensure t)
---
+++ lisp/startup.el
@@ -0,0 +1,26 @@
+;; Remove toolbar
+(tool-bar-mode -1)
+(display-time-mode 1)
+
+;; Change backup file Location
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+
+;; Sentence end to single space
+(setq sentence-end-double-space nil)
+
+;; Save history
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
+
+(provide 'startup)
